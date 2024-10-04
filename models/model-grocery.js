5803c2848b5e3002b78f180711ac8cd22958acb0
realpolya <ps2780@columbia.edu>
2024-10-04 10:30:04 -0400

    add grocery model

---
+++ models/model-grocery.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+export default mongoose.model('Grocery', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    seller: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 1
+    },
+    listed: {
+        type: Boolean,
+        required: true,
+    },
+    date: {
+        type: Date, 
+        default: Date.now,
+    },
+    image: String,
+    info: String,
+    from: String,
+    resupplied: {
+        type: Boolean,
+        required: true,
+    },
+    frequency: Number,
+    halloween: {
+        type: Boolean,
+        required: true,
+    }
+}));
