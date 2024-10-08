dd8590e9bb8888109daeaeb0588711417e5cc1be
realpolya <ps2780@columbia.edu>
2024-10-07 23:11:56 -0400

    add displaying amounts to the cart controller

--- controllers/cart.js
+++ controllers/cart.js
@@ -11,8 +11,43 @@ const router = Router();
 
 /* --------------------------------Functions--------------------------------*/
 
+// TODO: move cart display function here – remove it from POST route
+const displayCart = async (cart) => {
+    
+    // get id keys from the array
+    const cartItemArray = cart[0].items.map((item) => [item.id, item.quantity])
+        
+    // array of grocery ids
+    const idArray = cart[0].items.map((item) => item.id);
 
+    // find groceries corresponding to idArray
+    const groceries = await Grocery.find({ _id: {$in: idArray} });
 
+    // create itemArray of names and quantities
+    const itemArray = cartItemArray.map((item) => {
+        const match = groceries.find(grocery => JSON.stringify(grocery._id) === JSON.stringify(item[0]));
+        if (match) {
+            console.log("found a match")
+            console.log("item is ", item);
+            let total = +item[1] * +match.price;
+            return { id: match._id, name: match.name, quantity: item[1], price: match.price, total };
+        }
+    }).filter(item => item !== undefined);
+
+    console.log("Done with loop");
+    console.log(itemArray);
+
+    return itemArray;
+};
+
+const calculateTotal = (itemArray) => {
+    // TODO: display total
+    let totalAmount = itemArray.reduce((arg, item) => {
+        return arg + item.total;
+    }, 0)
+    totalAmount = Math.trunc(totalAmount * 100) / 100;
+    return totalAmount;
+}
 
 /* --------------------------------Routes--------------------------------*/
 
@@ -42,20 +77,29 @@ router.get('/', async (req, res) => {
             if (match) {
                 console.log("found a match")
                 console.log("item is ", item);
-                return { name: match.name, quantity: item[1] };
+                let total = +item[1] * +match.price;
+                return { id: match._id, name: match.name, quantity: item[1], price: match.price, total };
             }
         }).filter(item => item !== undefined);
 
         console.log("Done with loop");
         console.log(itemArray);
 
+        // const itemArray = displayCart(cart);
+
         // TODO: display total
+        let totalAmount = itemArray.reduce((arg, item) => {
+            return arg + item.total;
+        }, 0)
+        totalAmount = Math.trunc(totalAmount * 100) / 100
+
+        // const totalAmount = calculateTotal(itemArray);
 
         // message
         let message;
 
         // render
-        res.render('templates/shopper/cart.ejs', { user, cart, itemArray, message })
+        res.render('templates/shopper/cart.ejs', { user, cart, itemArray, message, totalAmount })
     } catch (err) {
         console.error(err);
     }
@@ -113,7 +157,8 @@ router.post('/:id', async (req, res) => {
             if (match) {
                 console.log("found a match")
                 console.log("item is ", item);
-                return { name: match.name, quantity: item[1] };
+                let total = +item[1] * +match.price;
+                return { name: match.name, quantity: item[1], price: match.price, total };
             }
         }).filter(item => item !== undefined);
 
@@ -121,9 +166,13 @@ router.post('/:id', async (req, res) => {
         console.log(itemArray);
 
         // TODO: display total
+        let totalAmount = itemArray.reduce((arg, item) => {
+            return arg + item.total;
+        }, 0)
+        totalAmount = Math.trunc(totalAmount * 100) / 100;
 
         // render
-        res.render('templates/shopper/cart.ejs', { user, cart, itemArray, message });
+        res.render('templates/shopper/cart.ejs', { user, cart, itemArray, message, totalAmount });
 
     } catch (err) {
         console.error(err);
@@ -131,68 +180,68 @@ router.post('/:id', async (req, res) => {
     
 })
 
-// PUT remove item from cart
-router.post('/:id/inactive', async (req, res) => {
+// // PUT remove item from cart
+// router.post('/:id/inactive', async (req, res) => {
 
-    const id = req.params.id;
+//     const id = req.params.id;
     
-    try {
-        const user = await User.findById(req.session.user._id);
+//     try {
+//         const user = await User.findById(req.session.user._id);
 
-        // find listing to archive
-        const listing = await Grocery.findById(id);
+//         // find listing to archive
+//         const listing = await Grocery.findById(id);
         
-        // archive listing
-        listing.listed = false;
+//         // archive listing
+//         listing.listed = false;
 
-        // save the listing
-        await listing.save();
+//         // save the listing
+//         await listing.save();
 
-        // find all listings
-        const listings = await Grocery.find({ seller: user._id, listed: true });
+//         // find all listings
+//         const listings = await Grocery.find({ seller: user._id, listed: true });
 
-        const message = "The following listing has been archived:"
-        const grocer = { archived: false }; // is grocer looking at archived items?
+//         const message = "The following listing has been archived:"
+//         const grocer = { archived: false }; // is grocer looking at archived items?
 
-        res.render('templates/grocer/listings.ejs', { user, listings, message, listing, grocer });
+//         res.render('templates/grocer/listings.ejs', { user, listings, message, listing, grocer });
 
-    } catch (err) {
-        console.error(err);
-    }
+//     } catch (err) {
+//         console.error(err);
+//     }
     
-})
+// })
 
-// PUT checkout cart
-router.post('/:id/relist', async (req, res) => {
+// // PUT checkout cart
+// router.post('/:id/relist', async (req, res) => {
 
-    const id = req.params.id;
+//     const id = req.params.id;
     
-    try {
-        const user = await User.findById(req.session.user._id);
+//     try {
+//         const user = await User.findById(req.session.user._id);
 
-        // find listing to archive
-        const listing = await Grocery.findById(id);
+//         // find listing to archive
+//         const listing = await Grocery.findById(id);
         
-        // archive listing
-        listing.listed = true;
+//         // archive listing
+//         listing.listed = true;
 
-        // save the listing
-        await listing.save();
+//         // save the listing
+//         await listing.save();
 
-        // find all listings
-        const listings = await Grocery.find({ seller: user._id, listed: true });
+//         // find all listings
+//         const listings = await Grocery.find({ seller: user._id, listed: true });
 
-        // message
-        const message = "The following listing has been reactivated:"
-        const grocer = { archived: false }; // is grocer dealing with archived items?
+//         // message
+//         const message = "The following listing has been reactivated:"
+//         const grocer = { archived: false }; // is grocer dealing with archived items?
 
-        res.render('templates/grocer/listings.ejs', { user, listings, message, listing, grocer });
+//         res.render('templates/grocer/listings.ejs', { user, listings, message, listing, grocer });
 
-    } catch (err) {
-        console.error(err);
-    }
+//     } catch (err) {
+//         console.error(err);
+//     }
     
-});
+// });
 
 
 /* --------------------------------Exports--------------------------------*/
